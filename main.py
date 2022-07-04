9ef977212aa1f2e604cf77f9db3645af934b2e23
Victoria Kotwicki <victoria.kotwicki@probablyrational.com>
2022-07-04 20:09:06 +0100

    Bare bones window class added - not working yet

--- main.py
+++ main.py
@@ -9,4 +9,28 @@ from tkinter.ttk import *
 # Add ability to pick room and automatically increments with next button if different room number not selected
 # save info to SQL database - find way to use joins?
 
+class App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs) # Creates tk.TK() instance
+        container = tk.Frame(self)
 
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1) # min size, priority
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {} # stores different "pages"
+
+        frame = MainMenu(container, self)
+
+        self.frames[MainMenu] = frame
+
+        # Assigns frame to specirfic position in grid
+        frame.grid(row=0, column=0, stinky="nswe") # nswe stretches to all sides of window
+
+        self.show_frame(MainMenu)
+
+
+if __name__=='__main__':
+    app = App()
+    app.mainloop()
