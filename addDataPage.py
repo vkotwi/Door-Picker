7fab3e9e29342f5d6c18c112bb6a61baf7ed46fe
Victoria Kotwicki <victoria.kotwicki@probablyrational.com>
2022-07-05 01:37:07 +0100

    Added radio button function

--- addDataPage.py
+++ addDataPage.py
@@ -1,18 +1,26 @@
 import tkinter as tk
 from tkinter import ttk
 
+import csv
+
 import pandas as pd
 
 events = {}
 # Gets names for buttons
-#data_names = pd.read_csv("events_and_rewards.txt")
+with open(r"events_and_rewards.txt") as file:
+    reader = csv.reader(file, delimiter=',', quotechar='"')
+    for r in reader:
+        events[r[0]] = []
+        for e in r[1:]:
+            events[r[0]].append(e.replace('\n', ''))
 
-#for i in data_names:
-#    print(i)
+print(events)
 
 
 # Page that allows user to add data without trying to work out which door to pick
 class AddDataPage(tk.Frame):
+    row_counter = 1
+    buttons= {}
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         tk.Frame.__init__(self, parent)
@@ -25,12 +33,26 @@ class AddDataPage(tk.Frame):
 
         lb_data.grid(row=0, column=2, pady=10, padx=10)
 
+        self.setup_radio_buttons("Events")
         #self.setup_checkboxes()
 
+    def setup_radio_buttons(self, key):
+        ttk.Label(self, text=key).grid(row=self.row_counter, column=0, pady=10, padx=10, sticky='w')
+        self.row_counter += 1
+        for ele in events[key]:
+            ttk.Radiobutton(self, text=ele, value=False).grid(row=self.row_counter, column=0, pady=5, padx=20, sticky='w')
+            self.row_counter += 1
+        # c1 = tk.Checkbutton(self, text='Python', variable=None, onvalue=1, offvalue=0, command=None)
+
     def setup_checkboxes(self):
-        for i in data_names.nrows:
-            c1 = tk.Checkbutton(self, text='Python', variable=None, onvalue=1, offvalue=0, command=None)
-        c1.pack()
+        counter = 1
+        for key in events:
+            ttk.Label(self, text=key).grid(row=counter, column=0, pady=10, padx=10, sticky='w')
+            counter += 1
+            for ele in events[key]:
+                tk.Radiobutton(self, text=ele).grid(row=counter, column=0, pady=5, padx=20, sticky='w')
+                counter += 1
+            # c1 = tk.Checkbutton(self, text='Python', variable=None, onvalue=1, offvalue=0, command=None)
 
     def gamblers_lure_rewards(self):
         pass
